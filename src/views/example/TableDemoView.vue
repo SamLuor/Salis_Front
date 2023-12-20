ce6ef3ad678749889d1f4e1b56c056612fe89aff
Samuel Lucas <samuelucasjob@gmail.com>
2023-12-20 17:56:39 -0300

    parsing to a setup script TableDemoView

--- src/views/example/TableDemoView.vue
+++ src/views/example/TableDemoView.vue
@@ -10,17 +10,19 @@
           <div class="container-filter">
             <InputText placeholder="Digite um Usuário" />
             <InputText placeholder="Digite uma empresa" />
-            <Dropdown :options="status" />
+            <Dropdown
+              v-model="filters['status']"
+              style="width: 150px"
+              :options="status"
+            />
           </div>
           <DataTable
-            v-model:filters="filters1"
             :value="customer1"
             :paginator="true"
             :rows="10"
             data-key="id"
             :row-hover="true"
             :loading="loading1"
-            :filters="filters1"
             scrollable
             responsive-layout="scroll"
             scroll-height="550px"
@@ -28,7 +30,6 @@
               'name',
               'country.name',
               'representative.name',
-              'balance',
               'status'
             ]"
           >
@@ -37,14 +38,6 @@
             <Column field="name" header="Usuário" style="min-width: 12rem">
               <template #body="{ data }">
                 {{ data.name }}
-              </template>
-              <template #filter="{ filterModel }">
-                <InputText
-                  v-model="filterModel.value"
-                  type="text"
-                  class="p-column-filter"
-                  placeholder="Search by name"
-                />
               </template>
             </Column>
             <Column
@@ -78,32 +71,6 @@
                   >{{ data.representative.name }}</span
                 >
               </template>
-              <template #filter="{ filterModel }">
-                <div class="mb-3 text-bold">Agent Picker</div>
-                <MultiSelect
-                  v-model="filterModel.value"
-                  :options="representatives"
-                  option-label="name"
-                  placeholder="Any"
-                  class="p-column-filter"
-                >
-                  <template #option="slotProps">
-                    <div class="p-multiselect-representative-option">
-                      <img
-                        :alt="slotProps.option.name"
-                        :src="'images/avatar/' + slotProps.option.image"
-                        width="32"
-                        style="vertical-align: middle"
-                      />
-                      <span
-                        style="margin-left: 0.5em; vertical-align: middle"
-                        class="image-text"
-                        >{{ slotProps.option.name }}</span
-                      >
-                    </div>
-                  </template>
-                </MultiSelect>
-              </template>
             </Column>
             <Column
               header="Date"
@@ -114,13 +81,6 @@
               <template #body="{ data }">
                 {{ formatDate(data.date) }}
               </template>
-              <template #filter="{ filterModel }">
-                <Calendar
-                  v-model="filterModel.value"
-                  date-format="mm/dd/yy"
-                  placeholder="mm/dd/yyyy"
-                />
-              </template>
             </Column>
           </DataTable>
         </div>
@@ -130,151 +90,52 @@
 </template>
 
 <script setup lang="ts">
-import { FilterMatchMode, FilterOperator } from 'primevue/api'
+import { FilterMatchMode } from 'primevue/api'
 import CustomerService from '@/service/CustomerService'
 import ProductService from '@/service/ProductService'
 import InputText from 'primevue/inputtext'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 
-const customer1 = ref(null)
-const filters1 = ref(null)
+const products = ref<any>(null)
+const customer1 = ref<any>(null)
 const loading1 = ref(true)
-const status = ref([
-  'unqualified',
-  'qualified',
-  'new',
-  'negotiation',
-  'renewal',
-  'proposal'
-])
+const status = ['Ativo', 'Desativado']
+const customerService = new CustomerService()
+const productService = new ProductService()
+const filters = ref({
+  global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+  'country.name': { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+  representative: { value: null, matchMode: FilterMatchMode.IN },
+  status: { value: null, matchMode: FilterMatchMode.EQUALS }
+})
 
-export default {
-  data() {
-    return {
-      statuses: [
-        'unqualified',
-        'qualified',
-        'new',
-        'negotiation',
-        'renewal',
-        'proposal'
-      ],
-      representatives: [
-        { name: 'Amy Elsner', image: 'amyelsner.png' },
-        { name: 'Anna Fali', image: 'annafali.png' },
-        { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
-        { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
-        { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
-        { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
-        { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
-        { name: 'Onyama Limba', image: 'onyamalimba.png' },
-        { name: 'Stephen Shaw', image: 'stephenshaw.png' },
-        { name: 'XuXue Feng', image: 'xuxuefeng.png' }
-      ]
-    }
-  },
-  customerService: null,
-  productService: null,
-  created() {
-    this.customerService = new CustomerService()
-    this.productService = new ProductService()
-    this.initFilters1()
-  },
-  mounted() {
-    this.productService
-      .getProductsWithOrdersSmall()
-      .then((data: any) => (this.products = data))
-    this.customerService.getCustomersLarge().then((data: any) => {
-      this.customer1 = data
-      this.loading1 = false
-      this.customer1.forEach(
-        (customer: { date: string | number | Date }) =>
-          (customer.date = new Date(customer.date))
-      )
-    })
-    this.customerService
-      .getCustomersLarge()
-      .then((data: any) => (this.customer2 = data))
-    this.customerService
-      .getCustomersMedium()
-      .then((data: any) => (this.customer3 = data))
-    this.loading2 = false
-  },
-  methods: {
-    initFilters1() {
-      this.filters1 = {
-        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        name: {
-          operator: FilterOperator.AND,
-          constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }]
-        },
-        'country.name': {
-          operator: FilterOperator.AND,
-          constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }]
-        },
-        representative: { value: null, matchMode: FilterMatchMode.IN },
-        date: {
-          operator: FilterOperator.AND,
-          constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }]
-        },
-        balance: {
-          operator: FilterOperator.AND,
-          constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }]
-        },
-        status: {
-          operator: FilterOperator.OR,
-          constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }]
-        },
-        activity: { value: null, matchMode: FilterMatchMode.BETWEEN },
-        verified: { value: null, matchMode: FilterMatchMode.EQUALS }
-      }
-    },
-    clearFilter1() {
-      this.initFilters1()
-    },
-    expandAll() {
-      this.expandedRows = this.products.filter((p: { id: any }) => p.id)
-    },
-    collapseAll() {
-      this.expandedRows = null
-    },
-    formatCurrency(value: {
-      toLocaleString: (
-        arg0: string,
-        arg1: { style: string; currency: string }
-      ) => any
-    }) {
-      return value.toLocaleString('en-US', {
-        style: 'currency',
-        currency: 'USD'
-      })
-    },
-    formatDate(value: {
-      toLocaleDateString: (
-        arg0: string,
-        arg1: { day: string; month: string; year: string }
-      ) => any
-    }) {
-      return value.toLocaleDateString('en-US', {
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric'
-      })
-    },
-    calculateCustomerTotal(name: any) {
-      let total = 0
-      if (this.customer3) {
-        for (const customer of this.customer3) {
-          if (customer.representative.name === name) {
-            total++
-          }
-        }
-      }
-
-      return total
-    }
-  }
+const formatDate = (value: {
+  toLocaleDateString: (
+    arg0: string,
+    arg1: { day: string; month: string; year: string }
+  ) => any
+}) => {
+  return value.toLocaleDateString('en-US', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric'
+  })
 }
+
+onMounted(() => {
+  productService
+    .getProductsWithOrdersSmall()
+    .then((data: any) => (products.value = data))
+  customerService.getCustomersLarge().then((data: any) => {
+    customer1.value = data
+    loading1.value = false
+    customer1.value.forEach(
+      (customer: { date: string | number | Date }) =>
+        (customer.date = new Date(customer.date))
+    )
+  })
+})
 </script>
 
 <style scoped lang="scss">
